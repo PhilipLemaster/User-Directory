a252a06603bcd395343cf9cbaf2e68357eb13f75
Philip Lemaster <philiplemaster@yahoo.com>
2020-06-27 22:18:24 -0400

    Updates

---
+++ precache-manifest.bfa743a36faa0dd48cdb9a7b518352ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7bbb87bebeaaa9eed49a27a02f8bbf",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "4a7a67f2a762c420e4c5",
+    "url": "/user-directory/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "e9078deac3bb47183d4a",
+    "url": "/user-directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4a7a67f2a762c420e4c5",
+    "url": "/user-directory/static/js/2.3881103e.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/user-directory/static/js/2.3881103e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9078deac3bb47183d4a",
+    "url": "/user-directory/static/js/main.03292216.chunk.js"
+  },
+  {
+    "revision": "e0c8332cf67520a1e021",
+    "url": "/user-directory/static/js/runtime-main.2b603db7.js"
+  }
+]);
